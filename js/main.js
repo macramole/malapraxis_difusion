3c51642d842a718d752fdb6d402e177c4a45f2d7
leandro <leandrogarber@gmail.com>
2017-09-21 15:26:29 -0300

    citas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var CANT_IMAGENES = 70;
 var CANT_IMAGENES_GRANDES = 10;
+var CANT_CITAS = 2;
 // var IMAGENES_GRANDES = [ 7, 18 ];
 
 var cantImagesLoaded = 0;
@@ -14,12 +15,14 @@ $(function() {
 
 function addImages() {
   var currentImagenGrande = 1;
+  var currentCitas = 1;
+  CANT_IMAGENES_TOTALES = CANT_IMAGENES + CANT_IMAGENES / 2;
 
-  for ( var i = 1 ; i <= CANT_IMAGENES ; i++ ) {
-      var $img = $("<img>").attr("src", "images/fotos/" + i + ".JPG");
+  for ( var i = 1 ; i <= CANT_IMAGENES_TOTALES ; i++ ) {
+      var $img = $("<img>").attr("src", "images/fotos/" + (i % CANT_IMAGENES) + ".JPG");
       $("#imgs .wrapper").append($img);
 
-      if ( i % ( Math.floor(CANT_IMAGENES/CANT_IMAGENES_GRANDES ) ) == 0 ) {
+      if ( i % 11 == 0 ) {
           if ( currentImagenGrande <= CANT_IMAGENES_GRANDES ) {
               var $imgGrande = $("<img>").attr("src", "images/fotos/grandes/" + currentImagenGrande + ".JPG");
               $imgGrande.addClass("grande");
@@ -27,18 +30,22 @@ function addImages() {
               currentImagenGrande++;
           }
       }
+      if ( i % 33 == 0 ) {
+          if ( currentCitas <= CANT_CITAS ) {
+              var $img = $("<img>").attr("src", "images/fotos/citas/" + currentCitas + ".PNG");
+              $img.addClass("cita");
+              $("#imgs .wrapper").append($img);
+              currentCitas++;
+          }
+      }
   }
-
-  var $img = $("<img>").attr("src", "images/fotos/citas/" + 1 + ".PNG");
-  $img.addClass("cita");
-  $("#imgs .wrapper").append($img);
 
   for ( var i = 1 ; i <= Math.floor(CANT_IMAGENES / 2) ; i++ ) {
       var $img = $("<img>").attr("src", "images/fotos/" + i + ".JPG");
       $("#imgs .wrapper").append($img);
   }
 
-  $('#imgs .wrapper').masonry({
+  $('#imgs .wrapper').packery({
     // options
     itemSelector: '#imgs .wrapper img',
     percentPosition: true,
@@ -50,13 +57,13 @@ function addImages() {
 
   $('#imgs .wrapper').imagesLoaded()
        .always( function( instance ) {
-           $('#imgs .wrapper').masonry('layout');
+           $('#imgs .wrapper').packery('layout');
        })
       .progress( function( instance, image ) {
           cantImagesLoaded++;
 
           if ( cantImagesLoaded % 15 == 0 ) {
-              $('#imgs .wrapper').masonry('layout');
+              $('#imgs .wrapper').packery('layout');
           }
       });
   ;
